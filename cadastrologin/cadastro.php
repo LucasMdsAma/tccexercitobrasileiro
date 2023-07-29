38b231aaf995ce1ed7d67cf4424805fc9405c9c1
LucasMdsAma <lucasmedeirosama@gmail.com>
2023-07-28 22:56:15 -0300

    atualização index, suporte, cadastro, login

--- cadastrologin/cadastro.php
+++ cadastrologin/cadastro.php
@@ -1,3 +1,24 @@
 <?php
-echo "teste";
+// if (isset($_POST['cadastrar'])) {
+//    $email = $_POST['ncadastroemail'];
+//    $senha = $_POST['ncadastrosenha'];
+//    $checksenha = $_POST['nconfirmarsenha'];
+// }
+
+// if ($senha != $checksenha) {
+//    die("As senhas não correspondem, confirme se digitou as duas igualmente.")
+// }
+
+// $host = "localhost";
+// $banco = "usuario";
+// $user = "root";
+// $senhauser = "";
+
+// $con = mysqli_connect($host, $banco, $user, $senhauser);
+
+// if(!$con) {
+//    die("Conexão falhou" . mysqli_connect_error());
+// }
+
+ echo "TESTE";
 ?>
