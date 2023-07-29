bb6415edc1a8e01c2b8f9e0a89b5408f706222f5
LucasMdsAma <lucasmedeirosama@gmail.com>
2023-07-28 21:23:02 -0300

    TESTE PHP

---
+++ cadastrologin/cadastro.php
@@ -0,0 +1,22 @@
+<?php
+if (isset($_POST['cadastrar'])) {
+   $email = $_POST['ncadastroemail'];
+   $senha = $_POST['ncadastrosenha'];
+   $checksenha = $_POST['nconfirmarsenha'];
+}
+
+if ($senha != $checksenha) {
+   die("As senhas não correspondem, confirme se digitou as duas igualmente.")
+}
+
+$host = "localhost";
+$banco = "usuario";
+$user = "root";
+$senhauser = "";
+
+$con = mysqli_connect($host, $banco, $user, $senhauser);
+
+if(!$con) {
+   die("Conexão falhou" . mysqli_connect_error());
+}
+?>
